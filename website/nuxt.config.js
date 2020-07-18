5164c67c4da3cd6e51208296b9096f6505e0f873
zhuqingguang03 <zhuqingguang03@kuaishou.com>
2020-07-18 23:58:28 +0800

    feat: 增加百度统计&添加网站标题

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -10,6 +10,14 @@ export default {
     css: [
         '~assets/css/tailwind.css', 
     ],
+    head: {
+        title: '资源收集站',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'keywords', content: 'IT, 程序员，编程，编程工具，免费课程，软件，学习资源' },
+            { name: 'description', content: '这是一个资源集合网址，收录了阮一峰老师的每周期刊中的工具，资源，课程和软件信息' }
+        ],
+    },
     build: {
         postcss: {
           plugins: {
