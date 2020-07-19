d3cd7371d220478c6dc5443c076492fca8315942
zhuqingguang03 <zhuqingguang03@kuaishou.com>
2020-07-19 10:33:56 +0800

    chore: 更改百度统计的接入方式

--- website/nuxt.config.js
+++ website/nuxt.config.js
@@ -11,12 +11,28 @@ export default {
         '~assets/css/tailwind.css', 
     ],
     head: {
+        __dangerouslyDisableSanitizers: ['script'],
         title: '资源收集站',
         meta: [
             { charset: 'utf-8' },
             { name: 'keywords', content: 'IT, 程序员，编程，编程工具，免费课程，软件，学习资源' },
             { name: 'description', content: '这是一个资源集合网址，收录了阮一峰老师的每周期刊中的工具，资源，课程和软件信息' }
         ],
+        script: [
+            {
+                innerHTML: `
+                    var _hmt = _hmt || [];
+                    (function() {
+                    var hm = document.createElement("script");
+                    hm.src = "https://hm.baidu.com/hm.js?abf89bf01510e02072a8b2241502b619";
+                    var s = document.getElementsByTagName("script")[0]; 
+                    s.parentNode.insertBefore(hm, s);
+                    })();
+                `,
+                type: 'text/javascript',
+                charset: 'utf-8'
+            }
+        ]
     },
     build: {
         postcss: {
